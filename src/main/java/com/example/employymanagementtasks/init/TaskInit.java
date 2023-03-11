e8456e93e44c4e442120911a2b3c88e0d6b5da20
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-03-11 17:18:03 +0200

    Style App

--- src/main/java/com/example/employymanagementtasks/init/TaskInit.java
+++ src/main/java/com/example/employymanagementtasks/init/TaskInit.java
@@ -9,7 +9,7 @@ public class TaskInit implements CommandLineRunner {
 
     private final TaskPriorityService taskService;
 
-    public TaskInit(TaskPriorityService taskService) {
+    public TaskInit(TaskPriorityService taskService)     {
         this.taskService = taskService;
     }
 
